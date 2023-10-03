7c2f73809481e0a13a2572986faba95c8accd748
jkwon-dev <kwonejk@gmail.com>
2023-10-03 12:33:34 +0800

    Initial Commit

---
+++ LocationApp2/LocationApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp2App.swift
+//  LocationApp2
+//
+//  Created by kwon eunji on 2023/10/03.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
